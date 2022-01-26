0ad0591af4033f00d53c6068f6c20527fe8312b5
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-01-26 11:41:29 -0300

    Demonstrate jsx style
    Using react jsx style, css in javascript, demonstrate css usage on an h1
    html tag inside the react HomePage component.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ function HomePage() {
 		<div>
 			<h1>Boas Vindas de Volta!</h1>
 			<h2>Discord - Alura Matrix</h2>
+			<style jsx> {`
+				h1 {
+					color: red;
+				}
+			`} </style>
 		</div>
 	)
 }
